d21c7c57b75a00a155573c88e328f74e233622e9
alexRodgers <rodgersajb@gmail.com>
2022-04-27 15:36:09 -0400

    starter stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react'
+import axios from 'axios';
+
 
 function App() {
+  useEffect(() => {
+    axios({
+      url: "http://taco-randomizer.herokuapp.com/",
+      method: "GET",
+      dataResponse: "json",
+    
+    }).then((res) => {
+      console.log(res);
+    });
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>STONKS ONLY GO UP</h1>
     </div>
   );
 }
