7e4dad2caa96fc089a0721238f8b599ce724b3f1
alexRodgers <rodgersajb@gmail.com>
2022-04-27 20:52:55 -0400

    small set up stuff

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import './App.css';
 import { useEffect } from 'react'
 import axios from 'axios';
 
+// Things to figure out:
+  // How to navigate the canvas larger than the client viewport
+  // Board navigation
 
 function App() {
-  useEffect(() => {
-    axios({
-      url: "http://taco-randomizer.herokuapp.com/",
-      method: "GET",
-      dataResponse: "json",
-    
-    }).then((res) => {
-      console.log(res);
-    });
-  }, [])
+  
+  
   return (
     <div className="App">
     <h1>STONKS ONLY GO UP</h1>
