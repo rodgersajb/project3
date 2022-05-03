515e528356b5a217d71666a0b81659eebdec1a46
alexRodgers <rodgersajb@gmail.com>
2022-05-02 22:27:20 -0400

    Deleted, starting from scratch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import './App.css'
 import axios from 'axios';
 import { useEffect, useState } from 'react'
 import { getDatabase, ref, onValue, push, remove } from 'firebase/database'
 import firebase from './firebase';
+import Post from './Components/Post';
+
 
 // Connect Firebase
 // Import axios library
@@ -24,98 +26,15 @@ import firebase from './firebase';
 
 
 function App() {
-  const [posts, setPosts] = useState([])
-  const [userInput, setUserInput] = useState('')
-  useEffect(() => {
-    // create a variable that holds our database details
-    const database = getDatabase(firebase)
-
-    // create a variable that makes reference to our database
-    const dbRef = ref(database)
-
-    // add an Event listener to that variable that will fire from that database
-    onValue(dbRef, (response) => {
-      const newState = []
-      const data = response.val()
-
-      for (let key in data) {
-        newState.push({ id: key, name: data[key]}
-        )
-      }
-
-      setPosts(newState)
-      console.log(response.val());
-    })
-
-  }, [])
-
-// Create an event handler that will fire every time there is input
-
-const handleInputChange = (event) => {
-  setUserInput(event.target.value)
-  // telling react 
-  
-}
-
-const userSubmit = (event) => {
-  event.preventDefault();
-
-  // always need to create a reference to firebase
-  const database = getDatabase(firebase);
-  const dbRef = ref(database)
-
-  // push the value of userInput to the base
-  if(userInput) {
-
-    push( dbRef, userInput )
-  
-    setUserInput('');
-  } else {
-    alert('It can be anything but not nothing')
-
-  }
-}
-
-const removePost = (postId) => {
-  const database = getDatabase(firebase)
-  // I have no idea how we got to this part
-  const dbRef = ref(database, `/${postId}`)
-
-  // firebase method remove 👇👇
-  remove(dbRef)
-}
-
   return (
-    <div className="App">
-      <h1>Share Board</h1>
+    <div className="wrapper">
 
-      <form action="submit" onSubmit={(event) => { userSubmit(event)}}>
-        <label htmlFor="newPost">Write whatever you feel like writing</label>
-        <textarea type="text" 
-        rows={10}  
-        cols={50}   
-        id="newPost" 
-        onChange={handleInputChange}
-        value={userInput}
-        />
-        <button onClick={userSubmit}>Post!</button>
-      </form>
-      
-        <ul>
-          {posts.map((post) => {
-            
-            return (
-              <li key={post.id}>
-                <p>{post.name} - {post.key}</p>
-                <button onClick={() => removePost(post.id)}> Remove </button>
-              </li>
-            )
-          })}
-        </ul>
+    {/* <h1>hey</h1>
+
+    <Post /> */}
+
     </div>
-      
-    
-  );
+  )
 }
 
 export default App;
